bad5f33f35123adde1ac73488f3a3c4310c32459
Christopher C. Fernandes <christopher.webpg@gmail.com>
2018-02-12 22:51:20 -0300

    implementando o brodcasting

---
+++ app/Events/EnviarMensagem.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Chat\Events;
+
+
+use Chat\Models\Mensagem;
+use Chat\Models\User;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Queue\SerializesModels;
+
+
+class EnviarMensagem implements ShouldBroadcast
+{
+    use SerializesModels;
+
+    /** @var Mensagem */
+    public $mensagem;
+
+    /** @var User */
+    public $user;
+
+    /**
+     * EnviarMensagem constructor.
+     * @param User $user
+     */
+    public function __construct(Mensagem $mensagem)
+    {
+        $this->mensagem = $mensagem;
+        $this->user = auth()->user();
+    }
+
+
+    /**
+     * @return array|PresenceChannel
+     */
+    public function broadcastOn()
+    {
+       return new PresenceChannel("chat.show.{$this->mensagem->chat_id}");
+    }
+}
